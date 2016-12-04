2baf4e9aee7a410f731ae8bc689be496bde38610
CyanDark, Incorporated <admin@cyandark.com>
2016-12-04 01:09:53 -0600

    Logs Directory

--- src/debugger/autoload.php
+++ src/debugger/autoload.php
@@ -13,6 +13,6 @@ require __DIR__ . '/src/tracy.php';
 
 use Tracy\Debugger;
 
-Debugger::enable(Debugger::DEVELOPMENT, dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'log');
+Debugger::enable(Debugger::DEVELOPMENT, dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'logs');
 Debugger::$strictMode = false;
 ?>

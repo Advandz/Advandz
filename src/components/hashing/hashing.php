bdd1fad063c2d72f3a8c333577165d1c7b578ab6
Diego Moreno <yeyoror@gmail.com>
2016-12-06 22:02:42 -0500

    Update Hashing Component

--- src/components/hashing/hashing.php
+++ src/components/hashing/hashing.php
@@ -3,12 +3,12 @@
  * Provides a keyed hash using HMAC and the Hash of a specific data.
  *
  * @package Advandz
- * @subpackage Advandz.components.session
+ * @subpackage Advandz.components.hashing
  * @copyright Copyright (c) 2012-2017 CyanDark, Inc. All Rights Reserved.
  * @license https://opensource.org/licenses/MIT The MIT License (MIT)
  * @author The Advandz Team <team@advandz.com>
  */
-class hashing{
+class Hashing {
     /**
      * Calculate a keyed hash using HMAC.
      *
@@ -16,19 +16,20 @@ class hashing{
      * @param mixed $data Data to be hashed.
      * @param string $key Shared secret key.
      * @param boolean $raw Outputs the hash as raw binary data.
-     * @return string $hmac Return a keyed hash using HMAC.
+     * @return string Return a keyed hash using HMAC.
      */
     public function hmacHash($algorithm, $data, $key, $raw=false){
         $hmac = hash_hmac($algorithm, $data, $key, $raw);
         return $hmac;
     }
+
     /**
      * Calculate the Hash of a specific data.
      *
      * @param string $algorithm Hashing algorithm.
      * @param mixed $data Data to be hashed.
      * @param boolean $raw Outputs the hash as raw binary data.
-     * @return string $hash Return the Hash of a specific data.
+     * @return string Return the Hash of a specific data.
      */
     public function hash($algorithm, $data, $raw = false){
         $hash = hash($algorithm, $data, $raw);

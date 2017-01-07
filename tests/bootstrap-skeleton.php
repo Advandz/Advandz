8033be8a7ccb4918019440dc809e346942f5bf8d
CyanDark, Inc <abdy@MacBook-Pro.local>
2017-01-06 23:48:01 -0600

    Source Path Updated in PHP Unit Tests

--- tests/bootstrap-skeleton.php
+++ tests/bootstrap-skeleton.php
@@ -1,3 +1,3 @@
 <?php
-$src_path = dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . "src";
+$src_path = dirname(dirname(__FILE__));
 require_once $src_path . DIRECTORY_SEPARATOR . "lib" . DIRECTORY_SEPARATOR . "init.php";

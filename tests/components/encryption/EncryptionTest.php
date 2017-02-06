d47242446116d42d7501f1d84a6ed46f088a5719
CyanDark, Inc <abdy@MacBook-Pro.local>
2017-02-05 22:22:04 -0600

    PHP Test Unit for Encryption, Hashing and Password

---
+++ tests/components/encryption/EncryptionTest.php
@@ -0,0 +1,43 @@
+<?php
+
+class EncryptionTest extends PHPUnit_Framework_TestCase
+{
+    private $Encryption;
+    private $string;
+    private $encrypted;
+
+    public function setUp()
+    {
+        $this->Encryption = new Advandz\Component\Encryption();
+
+        $key = $this->Encryption->generateKey();
+        $this->Encryption->setKey($key);
+
+        $this->string = 'test';
+    }
+
+    /**
+     * @covers Encryption::encrypt
+     */
+    public function testEncrypt()
+    {
+        $this->encrypted = $this->Encryption->encrypt($this->string);
+        $this->assertTrue(is_string($this->encrypted));
+    }
+
+    /**
+     * @covers Encryption::decrypt
+     */
+    public function testDecrypt()
+    {
+        $this->assertEquals($this->string, $this->Encryption->decrypt($this->encrypted));
+    }
+
+    /**
+     * @covers Encryption::generateKey
+     */
+    public function testGenerateKey()
+    {
+        $this->assertTrue(is_string($this->Encryption->generateKey()));
+    }
+}
---
+++ tests/components/hashing/HashingTest.php
@@ -0,0 +1,47 @@
+<?php
+
+class HashingTest extends PHPUnit_Framework_TestCase
+{
+    private $Hashing;
+
+    public function setUp()
+    {
+        $this->Hashing = new Advandz\Component\Hashing();
+    }
+
+    /**
+     * @covers Hashing::hmacHash
+     */
+    public function testHmacHash()
+    {
+        $expected = '63d6baf65df6bdee8f32b332e0930669';
+        $this->assertEquals($expected, $this->Hashing->hmacHash('md5', 'test', 'secret'));
+    }
+
+    /**
+     * @covers Hashing::hash
+     */
+    public function testHash()
+    {
+        $expected = '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08';
+        $this->assertEquals($expected, $this->Hashing->hash('sha256', 'test'));
+    }
+
+    /**
+     * @covers Hashing::listHashAlgorithms
+     */
+    public function testListHashAlgorithms()
+    {
+        $this->assertTrue(is_array($this->Hashing->listHashAlgorithms()));
+    }
+
+    /**
+     * @covers Hashing::compareHash
+     */
+    public function testCompareHash()
+    {
+        $a = '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08';
+        $b = '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08';
+        $this->assertTrue($this->Hashing->compareHash($a, $b));
+    }
+}

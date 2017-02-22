0ce21c276ac4543d18539dde9bcf961ec3ec8ce6
Abdy Franco <abdy@MacBook-Pro.local>
2017-02-21 22:45:25 -0600

    Cashier

---
+++ components/cashier/gateways/stripe_gateway.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Provides a number of methods for manage your billing subscriptions, with built-in gateways integrations.
+ *
+ * @package Advandz
+ * @subpackage Advandz.components.cashier.stripe
+ * @copyright Copyright (c) 2012-2017 CyanDark, Inc. All Rights Reserved.
+ * @license https://opensource.org/licenses/MIT The MIT License (MIT)
+ * @author The Advandz Team <team@advandz.com>
+ */
+
+namespace Advandz\Component\Cashier;
+
+use Stripe;
+
+class StripeGateway
+{
+    
+}
